8cb27f0d43fbc54915eea733e8eea591d0e39293
cactusboi <yaseminaysel@yahoo.com>
2019-02-01 16:16:47 -0500

    animal screen updates

---
+++ Assets/Scripts/AnimalInfo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+
+[System.Serializable]
+public class AnimalInfo
+{
+
+    public int id;
+    public string Name;
+    public string Species;
+    public string Description;
+
+    public static AnimalInfo CreateFromJSON(string AnimalList)
+    {
+        return JsonUtility.FromJson<AnimalInfo>(AnimalList);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/AnimalUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalUI : MonoBehaviour
+{
+    public List<Item> Items = new List<Item>();
+    public Transform ItemsParent;
+    public AnimalSlot[] itemSlots;
+
+    Inventory inventory;
+    
+
+    AnimalSlot[] slots;
+
+    void Start()
+    {
+        inventory = Inventory.instance;
+        inventory.onItemChangedCallback += UpdateUI;
+
+        slots = ItemsParent.GetComponentsInChildren<AnimalSlot>();
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inventory.Items.Count)
+            {
+                slots[i].AddItem(inventory.Items[i]);
+            }
+            else
+            {
+                Debug.Log("clearing inventory slots?");//slots[i].ClearSlot();
+            }
+
+        }
+        //Debug.Log("UPDATING UI");
+    }
+}
